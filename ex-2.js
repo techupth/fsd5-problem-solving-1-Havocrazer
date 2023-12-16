388043890d3b2cf1261ed335fddfe3a62b5e8673
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2023-12-17 00:19:23 +0700

    feat: add finish

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,22 @@
 
 const isPalindrome = function (s) {
   //Start Coding here
+  s = s.toLowerCase();
+  let normalWord = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "," && s[i] !== " " && s[i] !== ":") {
+      normalWord = s[i] + normalWord;
+    }
+  }
+  let backward = "";
+  for (let i = 0; i < normalWord.length; i++) {
+    backward = normalWord[i] + backward;
+  }
+  if (backward === normalWord) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
